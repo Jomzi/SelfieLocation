df96f5bfb77a591f4ac20223135f5087bdb42609
James O'Hanlon <James O'Hanlon>
2017-03-31 20:14:44 +0100

    Added Buttons Save Discard

--- SelfieLocation/MainPage.xaml.cs
+++ SelfieLocation/MainPage.xaml.cs
@@ -25,10 +25,13 @@ using Windows.Media.Devices;
 using Windows.Media.MediaProperties;
 using Windows.Phone.UI.Input;
 using Windows.System.Display;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Navigation;
-using Windows.ApplicationModel;
+using Windows.Devices.Geolocation;
+using System.Threading;
+using Windows.Media;
+using Windows.Graphics.Imaging;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.Storage.Streams;
+
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -39,8 +42,14 @@ namespace SelfieLocation
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        // Captures the value entered by user.
+        private uint _desireAccuracyInMetersValue = 0;
+        private CancellationTokenSource _cts = null;
+
         // Provides functionality to preview and capture the photograph
         private MediaCapture _mediaCapture;
+
+        private SoftwareBitmap _softwareBitMap;
 
         // This object allows us to manage whether the display goes to sleep 
         // or not while our app is active.
@@ -112,16 +121,89 @@ namespace SelfieLocation
             }
         }
 
+        private async void SaveSoftwareBitmapToFile(SoftwareBitmap softwareBitmap, StorageFile outputFile)
+        {
+            using (IRandomAccessStream stream = await outputFile.OpenAsync(FileAccessMode.ReadWrite))
+            {
+                // Create an encoder with the desired format
+                BitmapEncoder encoder = await BitmapEncoder.CreateAsync(BitmapEncoder.JpegEncoderId, stream);
+
+                // Set the software bitmap
+                encoder.SetSoftwareBitmap(softwareBitmap);
+
+                // Set additional encoding parameters, if needed
+                encoder.BitmapTransform.ScaledWidth = 320;
+                encoder.BitmapTransform.ScaledHeight = 240;
+                encoder.BitmapTransform.Rotation = Windows.Graphics.Imaging.BitmapRotation.Clockwise90Degrees;
+                encoder.BitmapTransform.InterpolationMode = BitmapInterpolationMode.Fant;
+                encoder.IsThumbnailGenerated = true;
+
+                try
+                {
+                    await encoder.FlushAsync();
+                }
+                catch (Exception err)
+                {
+                    switch (err.HResult)
+                    {
+                        case unchecked((int)0x88982F81): //WINCODEC_ERR_UNSUPPORTEDOPERATION
+                                                         // If the encoder does not support writing a thumbnail, then try again
+                                                         // but disable thumbnail generation.
+                            encoder.IsThumbnailGenerated = false;
+                            break;
+                        default:
+                            throw err;
+                    }
+                }
+
+                if (encoder.IsThumbnailGenerated == false)
+                {
+                    await encoder.FlushAsync();
+                }
+
+
+            }
+        }
         private async void snapButton_Click(object sender, RoutedEventArgs e)
-        { 
-            // This is where we want to save to.
-            var storageFolder = KnownFolders.SavedPictures;
+        {
+            if((String)snapButton.Content == "Discard")
+            {
+                PreviewControl.Visibility = Visibility.Visible;
+                StillImage.Visibility = Visibility.Collapsed;
+                snapButton.Content = "Capture";
 
-            // Create the file that we're going to save the photo to.
-            var file = await storageFolder.CreateFileAsync("sample.jpg", CreationCollisionOption.ReplaceExisting);
+            }
+            else
+            {
+                // Prepare and capture photo
+                var lowLagCapture = await _mediaCapture.PrepareLowLagPhotoCaptureAsync(ImageEncodingProperties.CreateUncompressed(MediaPixelFormat.Bgra8));
 
-            // Update the file with the contents of the photograph.
-            await _mediaCapture.CapturePhotoToStorageFileAsync(ImageEncodingProperties.CreateJpeg(), file);
+                var capturedPhoto = await lowLagCapture.CaptureAsync();
+                _softwareBitMap = capturedPhoto.Frame.SoftwareBitmap;
+
+                await lowLagCapture.FinishAsync();
+
+
+                PreviewControl.Visibility = Visibility.Collapsed;
+                StillImage.Visibility = Visibility.Visible;
+                snapButton.Content = "Discard";
+
+                if (_softwareBitMap.BitmapPixelFormat != BitmapPixelFormat.Bgra8 ||
+                    _softwareBitMap.BitmapAlphaMode == BitmapAlphaMode.Straight)
+                {
+                    _softwareBitMap = SoftwareBitmap.Convert(_softwareBitMap, BitmapPixelFormat.Bgra8, BitmapAlphaMode.Premultiplied);
+                }
+
+                var source = new SoftwareBitmapSource();
+                await source.SetBitmapAsync(_softwareBitMap);
+
+                // Set the source of the Image control
+                StillImage.Source = source;
+
+                
+            }
+
+
         }
 
         private void Dispose()
@@ -139,6 +221,8 @@ namespace SelfieLocation
             }
 
             _displayRequest.RequestRelease();
+
+           
         }
 
         private async Task SetPreviewRotationPropertiesAsync()
@@ -208,16 +292,108 @@ namespace SelfieLocation
         private async void sendButton_Click(object sender, RoutedEventArgs e)
         {
             // This is where we want to save to.
-            //StorageFolder storageFolder =KnownFolders.SavedPictures;
-            //StorageFile photoFile = await storageFolder.GetFileAsync("sample.jpg");
-            StorageFolder storageFolder = Windows.A
+            var storageFolder = KnownFolders.SavedPictures;
+
+            String date = DateTime.Now.ToString("MM-dd-yyyy.hh.mm.ss");
+            String fileName = "selfie-" + date + ".jpg";
+            
+
+            // Create the file that we're going to save the photo to.
+            var file = await storageFolder.CreateFileAsync(fileName, CreationCollisionOption.ReplaceExisting);
+
+            SaveSoftwareBitmapToFile(_softwareBitMap,file);
+
+            // Update the file with the contents of the photograph.
+
+            //await _mediaCapture.CapturePhotoToStorageFileAsync(ImageEncodingProperties.CreateJpeg(), file);
+
+
+            // This is where we want to save to.
+            
+           /* StorageFile photoFile = await storageFolder.GetFileAsync("sample.jpg");
+            //StorageFolder storageFolder = Windows.A
             var contactPicker = new Windows.ApplicationModel.Contacts.ContactPicker();
             contactPicker.SelectionMode = Windows.ApplicationModel.Contacts.ContactSelectionMode.Fields;
             contactPicker.DesiredFieldsWithContactFieldType.Add(Windows.ApplicationModel.Contacts.ContactFieldType.Email);
             Windows.ApplicationModel.Contacts.Contact recipient = await contactPicker.PickContactAsync();
 
-            await ComposeEmail(recipient, chatBox.Text, photoFile);
+            await ComposeEmail(recipient, chatBox.Text, photoFile);*/
 
+        }
+
+        async private void GetGeolocationButton_Click(object sender, RoutedEventArgs e)
+        
+        {
+            GetGeolocationButton.IsEnabled = false;
+            CancelGetGeolocationButton.IsEnabled = true;
+            //LocationDisabledMessage.Visibility = Visibility.Collapsed;
+
+            try
+            {
+                // Request permission to access location
+                var accessStatus = await Geolocator.RequestAccessAsync();
+
+                switch (accessStatus)
+                {
+                    case GeolocationAccessStatus.Allowed:
+
+                        // Get cancellation token
+                        _cts = new CancellationTokenSource();
+                        CancellationToken token = _cts.Token;
+
+                        //_rootPage.NotifyUser("Waiting for update...", NotifyType.StatusMessage);
+
+                        // If DesiredAccuracy or DesiredAccuracyInMeters are not set (or value is 0), DesiredAccuracy.Default is used.
+                        Geolocator geolocator = new Geolocator { DesiredAccuracyInMeters = _desireAccuracyInMetersValue };
+
+                        // Carry out the operation
+                        Geoposition pos = await geolocator.GetGeopositionAsync().AsTask(token);
+
+                        /* UpdateLocationData(pos);
+                         _rootPage.NotifyUser("Location updated.", NotifyType.StatusMessage);
+                         break;
+
+                     case GeolocationAccessStatus.Denied:
+                         _rootPage.NotifyUser("Access to location is denied.", NotifyType.ErrorMessage);
+                         LocationDisabledMessage.Visibility = Visibility.Visible;
+                         UpdateLocationData(null);
+                         break;
+
+                     case GeolocationAccessStatus.Unspecified:
+                         _rootPage.NotifyUser("Unspecified error.", NotifyType.ErrorMessage);
+                         UpdateLocationData(null);
+                         break;
+                         */
+                        break;
+                }
+            }
+            catch (TaskCanceledException)
+            {
+               // _rootPage.NotifyUser("Canceled.", NotifyType.StatusMessage);
+            }
+            catch (Exception ex)
+            {
+               // _rootPage.NotifyUser(ex.ToString(), NotifyType.ErrorMessage);
+            }
+            finally
+            {
+                _cts = null;
+            }
+
+            GetGeolocationButton.IsEnabled = true;
+            CancelGetGeolocationButton.IsEnabled = false;
+        }
+
+        private void CancelGetGeolocationButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (_cts != null)
+            {
+                _cts.Cancel();
+                _cts = null;
+            }
+
+            GetGeolocationButton.IsEnabled = true;
+            CancelGetGeolocationButton.IsEnabled = false;
         }
 
         
